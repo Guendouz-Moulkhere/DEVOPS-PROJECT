ee20de8e2470d8d839632122a2fc98d4e42c1562
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-11 22:28:47 -0400

    Update ImcAppApplicationTests.java

--- src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
+++ src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
@@ -10,8 +10,12 @@ public class ImcAppApplicationTests {
         double expectedImc = 22.86; // Valeur attendue avec deux chiffres après la virgule
         double delta = 0.01; // Tolérance de 0.01 pour la comparaison des valeurs décimales
 
-        ImcCalculator calculator = new ImcCalculator();
-        double actualImc = calculator.calculateImc(weight, height);
+        ImcDTO data = new ImcDTO();
+        data.setTaille(height);
+        data.setPoids(weight);
+
+        ImcService imcService = new ImcService();
+        double actualImc = Double.parseDouble(imcService.calculate(data).get("imc"));
 
         assertEquals(expectedImc, actualImc, delta);
     }
