b33e94ecd2c45187185a95e303e161edd91ae5d5
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-11 22:48:07 -0400

    Update ImcAppApplicationTests.java

--- src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
+++ src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
@@ -1,24 +1,13 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import devops.app.imcapp.ImcDTO;
-import devops.app.imcapp.ImcService;
+package devops.app.imcapp;
 
-public class ImcAppApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-    @Test
-    public void calculateImc() {
-        double height = 1.75;
-        double weight = 70;
-        double expectedImc = 22.86; // Valeur attendue avec deux chiffres après la virgule
-        double delta = 0.01; // Tolérance de 0.01 pour la comparaison des valeurs décimales
+@SpringBootTest
+class ImcAppApplicationTests {
 
-        ImcDTO data = new ImcDTO();
-        data.setTaille(height);
-        data.setPoids(weight);
+	@Test
+	void contextLoads() {
+	}
 
-        ImcService imcService = new ImcService();
-        double actualImc = Double.parseDouble(imcService.calculate(data).get("imc"));
-
-        assertEquals(expectedImc, actualImc, delta);
-    }
 }
