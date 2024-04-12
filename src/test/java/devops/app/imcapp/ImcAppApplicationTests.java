c0f7c8b9ba5afd293a5172f8bedee0cd6c457602
Guendouz-Moulkhere <78477568+Guendouz-Moulkhere@users.noreply.github.com>
2024-04-11 22:11:48 -0400

    Update ImcAppApplicationTests.java

--- src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
+++ src/test/java/devops/app/imcapp/ImcAppApplicationTests.java
@@ -1,13 +1,31 @@
 package devops.app.imcapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class ImcAppApplicationTests {
+public class ImcServiceTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ImcService imcService;
 
+    @Test
+    public void testCalculateImc() {
+        ImcDTO imcDTO = new ImcDTO();
+        imcDTO.setTaille(1.75); 
+        imcDTO.setPoids(70); 
+
+       
+        Map<String, String> result = imcService.calculate(imcDTO);
+
+       
+        assertEquals("24.49", result.get("imc")); 
+        assertEquals("Normale", result.get("avis")); 
+    }
 }
+
